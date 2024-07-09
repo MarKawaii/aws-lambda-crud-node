142d4021289dcf65a1f2895474dfa4b1600fb00a
Ignacio Veliz <101842633+MarKawaii@users.noreply.github.com>
2024-07-09 12:17:41 -0400

    Update almacenar-compensaciones.js

--- src/almacenar-compensaciones.js
+++ src/almacenar-compensaciones.js
@@ -18,32 +18,53 @@ const AlmacenarCompensaciones = async (event) => {
         }).promise();
 
         let item;
+        const createdAt = new Date().toISOString();
 
         if (result.Item) {
-            // Si el registro existe, agrega las compensaciones al año correspondiente
+            // Si el registro existe, verifica si el año ya está presente
             item = result.Item;
-            item[agno] = compensaciones;
+
+            if (!item.hasOwnProperty(agno)) {
+                // Si el año no existe, agregar las compensaciones para ese año
+                item[agno] = compensaciones;
+                item.updatedAt = createdAt;  // Agregar un campo de actualización opcional
+
+                // Actualiza el item en DynamoDB
+                await dynamodb.put({
+                    TableName: "CompensacionesTableNew",
+                    Item: item
+                }).promise();
+
+                return {
+                    statusCode: 200,
+                    body: JSON.stringify({ message: "Compensaciones agregadas para el nuevo año" }),
+                };
+            } else {
+                // Si el año ya existe, devolver un mensaje apropiado
+                return {
+                    statusCode: 400,
+                    body: JSON.stringify({ message: "El año ya existe para este id_sap" }),
+                };
+            }
         } else {
             // Si el registro no existe, crea uno nuevo
-            const createdAt = new Date().toISOString();
-
             item = {
                 id_sap: idSapString,
                 createdAt: createdAt,
                 [agno]: compensaciones
             };
+
+            // Inserta el nuevo item en DynamoDB
+            await dynamodb.put({
+                TableName: "CompensacionesTableNew",
+                Item: item
+            }).promise();
+
+            return {
+                statusCode: 200,
+                body: JSON.stringify({ message: "Nuevo registro de compensaciones creado exitosamente" }),
+            };
         }
-
-        // Inserta o actualiza el item en DynamoDB
-        await dynamodb.put({
-            TableName: "CompensacionesTableNew",
-            Item: item
-        }).promise();
-
-        return {
-            statusCode: 200,
-            body: JSON.stringify({ message: "Compensaciones creadas exitosamente" }),
-        };
     } catch (error) {
         return {
             statusCode: 500,
